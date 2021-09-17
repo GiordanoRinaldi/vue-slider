637902d74592f581bf4c7cb633b3d1e6971a3aab
GiordanoRinaldi <giordanorinaldi0@gmail.com>
2021-09-17 17:16:06 +0200

    aggiunte img + frecce e funz.vue

--- js/script.js
+++ js/script.js
@@ -1,9 +1,31 @@
 const app = new Vue({
     el: "#root",
     data: {
+        images: [
+            "https://www.vaporwaveitalia.it/wp-content/uploads/2018/05/cosa-%C3%A8-vaporwave-videogiochi-anni-80-90-aesthetics.png",
+            "https://www.newvibesinmusic.it/wp-content/uploads/maxresdefault.jpg",
+            "https://www.wallpaperuse.com/wallp/88-884241_m.jpg",
+            "img/img-4.jpg",
+            "img/img-5.jpg",
+        ],
+
+        currentImg: 0,
+
         
     },
     methods : {
+        imgPrima: function() {
+            this.currentImg--;
+            if (this.currentImg == -1 ) {
+                this.currentImg = this.images.length - 1;
+            }
+        },
+        imgDopo: function() {
+            this.currentImg++;
+            if (this.currentImg >= this.images.length ) {
+                this.currentImg = 0;
+            }
+        },
 
     }
 });
